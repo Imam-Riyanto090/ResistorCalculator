04bc2171857f722c716c55ba55a0d8f1a9b641ba
Danish Zaidan <95460214+Dzkamil@users.noreply.github.com>
2023-12-11 19:46:27 +0700

    membuat array dan menambahkan input warna gelang

--- resistor.c
+++ resistor.c
@@ -4,10 +4,17 @@
 int main()
 {
     int jumlahGelang;
-    char warna1[10], warna2[10], warna3[10], warna4[10], warna5[10];
+    char warna[] = {"hitam","coklat","merah","orange","kuning","hijau","biru","ungu","abu","putih"}; 
+    double toleransi;
+
 
     printf("Masukkan jumlah gelang (4 atau 5): ");
     scanf("%d", &jumlahGelang);
+    
+    for(int i = 0; i< jumlahGelang; i++){
+        printf("Masukkan warna gelang %d: ",i+1);
+        scanf("%s", warna);
+    }
+    
 
-    printf("tes");
 }
