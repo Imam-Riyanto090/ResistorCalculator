e7b2f93d22a02d6aa2994d000c7ed492f7a2607e
imam-riyanto090 <imamriyanto090@upi.edu>
2023-12-11 19:15:30 +0700

    Deklarasi inpt jumlah gelang

--- resistor.c
+++ resistor.c
@@ -6,5 +6,8 @@ int main()
     int jumlahGelang;
     char warna1[10], warna2[10], warna3[10], warna4[10], warna5[10];
 
-  printf("tes");
+    printf("Masukkan jumlah gelang (4 atau 5): ");
+    scanf("%d", &jumlahGelang);
+
+    printf("tes");
 }
